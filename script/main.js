cc77f261d8d7d243e3f50551648d7a5892f0737b
diian12 <121386156+diian12@users.noreply.github.com>
2022-12-28 20:24:58 +0700

    Update main.js

--- script/main.js
+++ script/main.js
@@ -97,7 +97,7 @@ const animationTimeline = () => {
     })
     .staggerTo(
       ".hbd-chatbox span",
-      1.5, {
+      1, {
         visibility: "visible",
       },
       0.05
@@ -105,7 +105,7 @@ const animationTimeline = () => {
     .to(".fake-btn", 0.1, {
       backgroundColor: "rgb(127, 206, 248)",
     },
-    "+=5")
+    "+=3")
     .to(
       ".four",
       0.5, {
@@ -113,7 +113,7 @@ const animationTimeline = () => {
         opacity: 0,
         y: -150
       },
-    "+=2")
+    "+=1")
     .from(".idea-1", 0.7, ideaTextTrans)
     .to(".idea-1", 0.7, ideaTextTransLeave, "+=2.5")
     .from(".idea-2", 0.7, ideaTextTrans)
